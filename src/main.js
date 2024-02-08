8b1d4b4d9ef8716ce864cdca6295966293fc8a10
yzbban <yzbbanban@live.com>
2024-02-08 23:48:27 +0800

    src index

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import './util'
 import './util/index2'
 Vue.config.productionTip = false
 Vue.use(ElementUI);
